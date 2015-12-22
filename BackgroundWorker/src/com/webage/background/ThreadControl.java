71401b1565f6a7751c6aecf6d06e02995a055c2b
Markus Borg <markus.borg@gmail.com>
2015-12-22 21:22:32 +0100

    Removed some whitespace

--- BackgroundWorker/src/com/webage/background/ThreadControl.java
+++ BackgroundWorker/src/com/webage/background/ThreadControl.java
@@ -22,7 +22,6 @@ import android.util.Log;
  */
 public class ThreadControl {
 	private final Lock lock = new ReentrantLock();
-
 	private Condition pauseCondition = lock.newCondition();
 	private boolean paused = false, cancelled = false;
 	
@@ -32,10 +31,8 @@ public class ThreadControl {
 	 */
 	public void pause() {
 		lock.lock();
-		
 		Log.v("ThreadControl", "Pausing");
 		paused = true;
-		
 		lock.unlock();
 	}
 	/**
@@ -86,7 +83,6 @@ public class ThreadControl {
 	 */
 	public void waitIfPaused() throws InterruptedException {
 		lock.lock();
-		
 		try {
 			while (paused && !cancelled) {
 					Log.v("ThreadControl", "Going to wait");
